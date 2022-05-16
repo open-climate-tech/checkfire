dd29dbd6b1fce347f8c5fdd7aacf72cd670f3c35
Mikol Graves <mikol@continuity.lan>
2022-05-16 15:43:16 -0700

    server: Use port 3141 instead of 5000 by default
    Port 5000 is used by macOS Control Center. See:
    https://developer.apple.com/forums/thread/682332

--- scripts/devStart.sh
+++ scripts/devStart.sh
@@ -2,4 +2,4 @@
 NODE_ENV=development node server-main.js &
 # then start react UI client
 # REACT_APP_BE_PORT=5000 node_modules/react-scripts/bin/react-scripts.js start
-REACT_APP_BE_PORT=5000 npx react-scripts start
+REACT_APP_BE_PORT=3141 npx react-scripts start
