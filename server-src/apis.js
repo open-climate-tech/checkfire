3c6105bca6929968473f533425bbbb7c7f1fd8ab
Kinshuk Govil <kinshuk@gmail.com>
2022-06-22 15:52:52 -0700

    API for getting active RX burns

--- server-src/apis.js
+++ server-src/apis.js
@@ -462,6 +462,23 @@ function initApis(config, app, db) {
       res.status(200).send('success').end();
     });
   });
+
+  /**
+   * Get a list of active RX/permitted Burns
+   */
+  app.get('/api/activeRxBurns', async (req, res) => {
+    logger.info('GET activeRxBurns');
+    const sourceActive = 'Active';
+    const sqlStr = `select info from rx_burns where source = '${sourceActive}' order by timestamp desc limit 1`
+    const dbRes = await db.query(sqlStr);
+    if (dbRes && (dbRes.length === 1)) {
+      const info = dbRes[0].Info || dbRes[0].info;
+      const activeBurnLocations = JSON.parse(info);
+      res.status(200).send(activeBurnLocations).end();
+    } else {
+      res.status(200).send([]).end();
+    }
+  });
 }
 
 exports.initApis = initApis;
